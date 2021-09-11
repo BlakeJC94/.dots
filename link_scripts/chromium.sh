15d2e939edbca5486590ac33087227e9af43d2fa
Blake <blakejamescook@gmail.com>
2021-09-11 22:27:37 +1000

    sync

--- link_scripts/chromium.sh
+++ link_scripts/chromium.sh
@@ -1,4 +1,7 @@
 mkdir -p $HOME/.local/share/keyrings
 
-ln -s $HOME/.dots/.spotify $HOME/.local/share/keyrings/spotify.keyring
-ln -s $HOME/.dots/.slack $HOME/.local/share/keyrings/slack.keyring
+unlink $HOME/.local/share/keyrings/spotify.keyring
+ln -s $HOME/Workspace/archived/keys/.spotify $HOME/.local/share/keyrings/spotify.keyring
+
+unlink $HOME/.local/share/keyrings/slack.keyring
+ln -s $HOME/Workspace/archived/keys/.slack $HOME/.local/share/keyrings/slack.keyring
