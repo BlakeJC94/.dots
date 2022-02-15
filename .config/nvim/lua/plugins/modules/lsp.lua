ede6bdf05e49e45867913fd9d68d269f03edcca7
Blake <blakejamescook@gmail.com>
2022-02-15 22:26:59 +1100

    sync

--- .config/nvim/lua/plugins/modules/lsp.lua
+++ .config/nvim/lua/plugins/modules/lsp.lua
@@ -130,10 +130,10 @@ M['kosayoda/nvim-lightbulb'] = {
     end
 }
 
-M["ray-x/lsp_signature.nvim"] = {
-    config = function()
-        require "lsp_signature".setup({})
-    end,
-}
+-- M["ray-x/lsp_signature.nvim"] = {
+--     config = function()
+--         require "lsp_signature".setup({})
+--     end,
+-- }
 
 return M
