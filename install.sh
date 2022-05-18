bc6a38a9f6b30f0fef1c9ee6a0410c1a7bf089ab
Blake <blakejamescook@gmail.com>
2022-05-18 11:28:03 +1000

    sync

--- install.sh
+++ install.sh
@@ -62,10 +62,30 @@ sudo apt install -y \
     zathura-ps
 
 # Install Python packages
+echo "===================================="
+echo "Installing Python build dependencies"
+sudo apt install -y \
+    build-essential \
+    gdb \
+    lcov \
+    pkg-config \
+    libbz2-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libgdbm-compat-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev libssl-dev \
+    lzma \
+    lzma-dev \
+    tk-dev \
+    uuid-dev \
+    zlib1g-dev
+
 echo "=============================="
 echo "Installing Python APT packages"
 sudo apt install -y \
-    libsqlite3-dev \
     python3-pip \
     python3-venv \
     python3-setuptools \
