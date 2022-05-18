2e9d17b7bd1b3158f732ef041485eb3d46f0bef4
Blake <blakejamescook@gmail.com>
2022-05-18 11:19:34 +1000

    sync

--- install.sh
+++ install.sh
@@ -65,6 +65,7 @@ sudo apt install -y \
 echo "=============================="
 echo "Installing Python APT packages"
 sudo apt install -y \
+    libsqlite3-dev \
     python3-pip \
     python3-venv \
     python3-setuptools \
