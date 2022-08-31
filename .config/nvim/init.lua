ae69875a343d5544f9a50703cb9d2fd3493362de
BlakeJC94 <blakejamescook@gmail.com>
2022-08-31 21:59:39 +1000

    clean

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,7 @@
 -- BLAKEJC94S NEOVIM INIT.LUA
 -- --------------------------
 
-local utils = require('utils')
-
+-- Load modules
 local NVIM_CONFIG = {
     plugins =      require("plugins"),
     options =      require("options"),
@@ -12,6 +11,18 @@ local NVIM_CONFIG = {
     mappings =     require("mappings"),
 }
 
+-- Setup packer
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_url = 'https://github.com/wbthomason/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    install_cmd = table.concat({'!git clone', packer_url, install_path}, ' ')
+    vim.fn.execute(install_cmd)
+end
+vim.cmd("packadd packer.nvim")
+
+-- Set leader key
 vim.g.mapleader = " "
-utils.setup_packer()
-for key, table in pairs(NVIM_CONFIG) do utils.load(key, table) end
+
+-- Set modules
+utils = require('utils')
+for key, table in pairs(NVIM_CONFIG) do utils[key](table) end
--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,19 +1,14 @@
-M = {}
+local SETTERS = {}
 
-DEFAULT_MAP_OPTS = {noremap = true, silent = true}
-DEFAULT_CMD_OPTS = {force = true}
-
--- -----------------------------
-SETTERS = {}
-SETTERS.options = function(_, options)
+SETTERS.options = function(options)
     for k, v in pairs(options) do vim.opt[k] = v end
 end
 
-SETTERS.functions = function(_, func)
+SETTERS.functions = function(func)
     _G.name = func
 end
 
-SETTERS.autocommands = function(_, autocommands)
+SETTERS.autocommands = function(autocommands)
     for name, augroup in pairs(autocommands) do
         local id = vim.api.nvim_create_augroup(name, {clear = true})
         for _, autocmd in pairs(augroup) do
@@ -29,7 +24,8 @@ SETTERS.autocommands = function(_, autocommands)
     end
 end
 
-SETTERS.commands = function(_, commands)
+SETTERS.commands = function(commands)
+    local DEFAULT_CMD_OPTS = {force = true}
     for name, command in pairs(commands) do
         if (type(command) == 'table') then
             local opts = vim.tbl_extend('force', DEFAULT_CMD_OPTS, command[2])
@@ -49,7 +45,8 @@ SETTERS.commands = function(_, commands)
 end
 
 -- TODO find a nicer solution than this, maybe a dir?
-SETTERS.mappings = function(_, mappings)
+SETTERS.mappings = function(mappings)
+    local DEFAULT_MAP_OPTS = {noremap = true, silent = true}
     for _, group_mappings in pairs(mappings) do
         for mode, mode_mappings in pairs(group_mappings) do
             for keys, mapping in pairs(mode_mappings) do
@@ -64,7 +61,7 @@ SETTERS.mappings = function(_, mappings)
     end
 end
 
-SETTERS.plugins = function(_, plugins)
+SETTERS.plugins = function(plugins)
     local status_ok, packer = pcall(require, "packer")
     if not status_ok then
         error("Packer not properly installed, skipping plugin loading.")
@@ -87,25 +84,4 @@ SETTERS.plugins = function(_, plugins)
     packer.install()
 end
 
-M.load = function(mode, table)
-    local setter = SETTERS[mode]
-    if setter == nil then error("Invalid `mode` passed to SETTERS") end
-    setter(nil, table)
-end
-
-M.setup_packer = function()
-    local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    local packer_url = 'https://github.com/wbthomason/packer.nvim'
-
-    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-        install_cmd = table.concat({'!git clone', packer_url, install_path}, ' ')
-        vim.fn.execute(install_cmd)
-    end
-
-    -- Add packer to managed plugins
-    vim.cmd("packadd packer.nvim")
-end
-
-
-
-return M
+return SETTERS
