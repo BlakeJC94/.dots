0f502a615198d2da7ef8512359418caf5b6739e3
BlakeJC94 <blakejamescook@gmail.com>
2022-09-07 17:23:09 +1000

    sync

--- .config/nvim/lua/addons/configs/_mason.lua
+++ .config/nvim/lua/addons/configs/_mason.lua
@@ -21,10 +21,26 @@ return {
             },
         })
 
+
         vim.api.nvim_create_autocmd("User", {
             pattern = "MasonToolsUpdateComplete",
             callback = function()
                 vim.schedule(print("mason-tool-installer has finished"))
+                local pyenv_root = os.getenv("PYENV_ROOT")
+                if pyenv_root then
+                    local fs = require("mason-core.fs")
+
+                    local mason_pylint = require("mason-core.path").bin_prefix("pylint")
+                    local pyenv_pylint = pyenv_root .. "/shims/pylint"
+
+                    -- Backup the mason bin
+                    fs.sync.rename(mason_pylint, require("mason-core.path").bin_prefix("pylint.mason"))
+
+                    -- Link mason bin to pyenv bin
+                    fs.sync.symlink(pyenv_pylint, mason_pylint)
+
+                    -- TODO check if pylint works in pyenv venv, install if not
+                end
             end,
         })
     end,
