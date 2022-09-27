f8f33563dddac50ab0b426a56bbfa3a750270521
BlakeJC94 <blakejamescook@gmail.com>
2022-09-28 08:51:06 +1000

    sync

--- bash/modules/tmux.sh
+++ bash/modules/tmux.sh
@@ -1,16 +1,14 @@
 # Automatically launch Tmux in Wezterm
-# if ! [ -z "$KITTY_WINDOW_ID" ] || ! [ -z "$WEZTERM_PANE" ] && [ -z "$TMUX" ]; then
-
-if [ -z "$TMUX" ] && [ -n "$WEZTERM_PANE" ] || [ -z "$TMUX" ] && [ -n "$KITTY_WINDOW_ID" ]; then
-    if [[ "$(command -v tmux)" ]]; then
-        N=$(tmux ls 2> /dev/null | grep -v attached | head -1 | cut -d: -f1)
-        if [[ -n $N ]]; then
-            exec tmux attach -t "${N}" && exit;
-            # echo "exec tmux attach -t" "${N}" "&& exit;"
-        else
-            exec tmux new-session && exit;
-            # echo "exec tmux new-session && exit;"
-        fi
+if [ -n "${WEZTERM_PANE}" ] || [ -n "${KITTY_WINDOW_ID}" ] && [ -z "${TMUX}" ] ; then
+    N=$(tmux ls 2> /dev/null | grep -v attached | head -1 | cut -d: -f1)
+    if [[ -n $N ]]; then
+        # exec tmux attach -t "${N}" && exit;
+        # echo "exec tmux attach -t" "${N}" "&& exit;"
+        tmux attach -t "${N}" "&& exit;"
+    else
+        # exec tmux new-session && exit;
+        # echo "exec tmux new-session && exit;"
+        tmux new-session && exit;
     fi
 fi
 
