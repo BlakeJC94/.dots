f200d131880ecbcdcfa8dc573a70ac1916b5b7a2
BlakeJC94 <blakejamescook@gmail.com>
2022-09-27 14:48:14 +1000

    sync

--- bash/modules/tmux.sh
+++ bash/modules/tmux.sh
@@ -1,12 +1,15 @@
-# Automatically launch Tmux in Kitty
-if ! [ -z "$KITTY_WINDOW_ID" ] || ! [ -z "$WEZTERM_PANE" ] && [ -z "$TMUX" ]; then
+# Automatically launch Tmux in Wezterm
+# if ! [ -z "$KITTY_WINDOW_ID" ] || ! [ -z "$WEZTERM_PANE" ] && [ -z "$TMUX" ]; then
+
+if [ -z "$TMUX" ] && [ -n "$WEZTERM_PANE" ] || [ -z "$TMUX" ] && [ -n "$KITTY_WINDOW_ID"]; then
     if [[ "$(command -v tmux)" ]]; then
-        N=$(tmux ls | grep -v attached | head -1 | cut -d: -f1)
-        if [[ ! -z $N ]]
-        then
-            exec tmux attach -t $N && exit;
+        N=$(tmux ls 2> /dev/null | grep -v attached | head -1 | cut -d: -f1)
+        if [[ -n $N ]]; then
+            exec tmux attach -t "${N}" && exit;
+            # echo "exec tmux attach -t" "${N}" "&& exit;"
         else
             exec tmux new-session && exit;
+            # echo "exec tmux new-session && exit;"
         fi
     fi
 fi
