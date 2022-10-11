b78d90468e00c2a35d6ac4358f5da78ae04143df
BlakeJC94 <blakejamescook@gmail.com>
2022-10-11 21:07:44 +1100

    sync

--- nvim/.config/nvim/lua/addons/mappings/arrows.lua
+++ nvim/.config/nvim/lua/addons/mappings/arrows.lua
@@ -1,41 +1,49 @@
 return {
-    [''] = {
+    [""] = {
         -- Disable arrow keys, git gud with hjkl! (and bwe)
-        ['<Left>']  = '',
-        ['<Down>']  = '',
-        ['<Up>']    = '',
-        ['<Right>'] = '',
-        ['<C-Left>']  = function() require('smart-splits').resize_left(8) end,
-        ['<C-Up>']    = function() require('smart-splits').resize_up(4) end,
-        ['<C-Down>']  = function() require('smart-splits').resize_down(4) end,
-        ['<C-Right>'] = function() require('smart-splits').resize_right(8) end,
-        ['<S-Left>']  = '',
-        ['<S-Down>']  = '',
-        ['<S-Up>']    = '',
-        ['<S-Right>'] = '',
-        ['<A-Left>']  = '',
-        ['<A-Down>']  = '',
-        ['<A-Up>']    = '',
-        ['<A-Right>'] = '',
+        ["<Left>"] = "",
+        ["<Down>"] = "",
+        ["<Up>"] = "",
+        ["<Right>"] = "",
+        ["<C-Left>"] = function()
+            require("smart-splits").resize_left(8)
+        end,
+        ["<C-Up>"] = function()
+            require("smart-splits").resize_up(4)
+        end,
+        ["<C-Down>"] = function()
+            require("smart-splits").resize_down(4)
+        end,
+        ["<C-Right>"] = function()
+            require("smart-splits").resize_right(8)
+        end,
+        ["<S-Left>"] = "",
+        ["<S-Down>"] = "",
+        ["<S-Up>"] = "",
+        ["<S-Right>"] = "",
+        ["<A-Left>"] = "",
+        ["<A-Down>"] = "",
+        ["<A-Up>"] = "",
+        ["<A-Right>"] = "",
     },
-    c = {
-        -- Intuitive vertical wildmenu controls
-        ['<Down>']  = {
-            map  = [[wildmenumode() ? "\<Right>" : "\<Down>"]],
-            opts = {expr=true, silent=false},
-        },
-        ['<Up>']    = {
-            map  = [[wildmenumode() ? "\<Left>" : "\<Up>"]],
-            opts = {expr=true, silent=false},
-        },
-        ['<Right>'] = {
-            map  = [[wildmenumode() ? "\<Down>" : "\<Right>"]],
-            opts = {expr=true, silent=false},
-        },
-        ['<Left>']  = {
-            map  = [[wildmenumode() ? "\<Up>" : "\<Left>"]],
-            opts = {expr=true, silent=false},
-        },
-    },
+    -- This seems to be broken now, but this is now handled properly by cmp
+    -- ['c'] = {
+    --     -- Intuitive vertical wildmenu controls
+    --     ['<Down>']  = {
+    --         map  = [[wildmenumode() ? "\<Right>" : "\<Down>"]],
+    --         opts = {expr=true, silent=false},
+    --     },
+    --     ['<Up>']    = {
+    --         map  = [[wildmenumode() ? "\<Left>" : "\<Up>"]],
+    --         opts = {expr=true, silent=false},
+    --     },
+    --     ['<Right>'] = {
+    --         map  = [[wildmenumode() ? "\<Down>" : "\<Right>"]],
+    --         opts = {expr=true, silent=false},
+    --     },
+    --     ['<Left>']  = {
+    --         map  = [[wildmenumode() ? "\<Up>" : "\<Left>"]],
+    --         opts = {expr=true, silent=false},
+    --     },
+    -- },
 }
-
