ccb62c41d6fd4ecb70107fa7a9473af086e9c03f
BlakeJC94 <blakejamescook@gmail.com>
2022-10-19 13:47:04 +1100

    sync

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -46,6 +46,14 @@ return {
             format = "$0",
         },
 
+        -- Linkify things that look like URLs with localhost.
+        -- E.g. http://127.0.0.1:8000 for a local development server,
+        -- or http://192.168.1.1 for the web interface of many routers.
+        {
+            regex = [[\b\w+://localhost:\d+[/]?\S*\b]],
+            format = "$0",
+        },
+
         -- Make username/project paths clickable. This implies paths like the following are for GitHub.
         -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
         -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
