dd4b39b95942bc28ee5f647a66b592c9b3d049b6
BlakeJC94 <blakejamescook@gmail.com>
2022-11-06 09:36:29 +1100

    sync

--- bash/modules/fortune.sh
+++ bash/modules/fortune.sh
@@ -1,11 +1,2 @@
 # print a fortune when starting up a bash session
 codehelp
-# FORTUNES_FILE="${HOME}/.dots/fortune/programming-tips"
-# if [[ "$(command -v fortune)" ]] && [[ -f ${FORTUNES_FILE} ]]; then
-#     if [[ "$(command -v colorize)" ]]; then
-#         fortune ${FORTUNES_FILE} | colorize magenta
-#     else
-#         fortune ${FORTUNES_FILE}
-#     fi
-# fi
-
