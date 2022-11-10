526a0beca511a45c11b3a9d39ca78032a3e90279
BlakeJC94 <blakejamescook@gmail.com>
2022-11-10 14:10:15 +1100

    sync

--- bash/modules/tmux.sh
+++ bash/modules/tmux.sh
@@ -4,7 +4,7 @@ if [ -n "${WEZTERM_PANE}" ] || [ -n "${KITTY_WINDOW_ID}" ] && [ -z "${TMUX}" ] ;
     if [[ -n $N ]]; then
         # exec tmux attach -t "${N}" && exit;
         # echo "exec tmux attach -t" "${N}" "&& exit;"
-        tmux attach -t "${N}" "&& exit;"
+        tmux attach -t "${N}" && exit;
     else
         # exec tmux new-session && exit;
         # echo "exec tmux new-session && exit;"
