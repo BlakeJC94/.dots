deee541d65ae0189726845e3636862d35b8affff
BlakeJC94 <blakejamescook@gmail.com>
2023-01-07 22:07:07 +1100

    moved local install scripts

--- zoot/base/local/ncspot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if ! [[ "$(command -v ncspot)" ]]; then
-    cargo install ncspot
-    sudo ln -s $HOME/.cargo/bin/ncspot $HOME/.local/bin/ncspot
-fi
-
--- zoot/base/local/nvm.sh
+++
@@ -1,9 +0,0 @@
-if ! [[ "$(command -v nvm)" ]]; then
-    export PROFILE=/dev/null
-    wget --progress bar -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-    export -n PROFILE
-fi
-nvm install node --latest-npm
