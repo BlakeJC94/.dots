46bb2d2ee1b80bf03b76f8de751d63301a183c88
BlakeJC94 <blakejamescook@gmail.com>
2023-01-09 08:17:04 +1100

    moved dirs and simplified installs further

---
+++ .scripts/install_scripts.sh
@@ -0,0 +1,5 @@
+shopt -s globstar
+for f in "${HOME}"/.install/*.sh; do
+    bash "${f}" 2>&1 > "${f}".log
+done
+
--- install.sh
+++ install.sh
@@ -3,19 +3,10 @@
 DOTS="${HOME}"/.dots
 
 
-run_scripts(){
-    shopt -s globstar
-    root_dir="${1}"
-    for f in "${root_dir}"/*.sh; do
-        bash "${f}" 2>&1 > "${f}".log
-    done
-}
+# bash "${DOTS}"/.scripts/link.sh
 
-# bash "${DOTS}"/scripts/link.sh
+# sudo bash "${DOTS}"/.scripts/apt.sh
+# bash "${DOTS}"/.scripts/flatpak.sh
 
-# sudo bash "${DOTS}"/scripts/apt.sh
-# bash "${DOTS}"/scripts/flatpak.sh
-
-# run sudo installs and user installs
-# sudo bash -c "$(declare -f run_scripts); run_scripts ${HOME}/.install-global"
-# bash -c "$(declare -f run_scripts); run_scripts ${HOME}/.install"
+# sudo bash "${DOTS}"/.scripts/install_global_scripts.sh
+# bash "${DOTS}"/.scripts/install_scripts.sh
