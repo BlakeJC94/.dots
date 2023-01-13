5edbc2b637c5b02fec2abbb9a5e863e4cc6c6a7b
BlakeJC94 <blakejamescook@gmail.com>
2023-01-13 19:34:37 +1100

    cleanup

--- _dropbox_cleanup.sh
+++
@@ -1,9 +0,0 @@
-
-DIRS=( Desktop Documents Music Pictures Videos Public  )
-pushd ~/Dropbox/
-for DIR in "${DIRS[@]}"; do
-    echo "$DIR"
-    unlink $HOME/$DIR
-    # ln -sf $HOME/Dropbox/Home-db/$DIR-db $HOME/$DIR
-done
-popd
