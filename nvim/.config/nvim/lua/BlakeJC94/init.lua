22b6936a4e2df4b424d998635e4d72d4e6fd1abd
BlakeJC94 <blakejamescook@gmail.com>
2023-01-22 10:52:15 +1100

    cleanup

--- nvim/.config/nvim/lua/BlakeJC94/init.lua
+++ nvim/.config/nvim/lua/BlakeJC94/init.lua
@@ -1,22 +1,3 @@
-local disabled_built_ins = {
-  'gzip',
-  'man',
-  'shada_plugin',
-  'tarPlugin',
-  'tar',
-  'zipPlugin',
-  'zip',
-  'netrwPlugin',
-  'tutor_mode_plugin',
-  'remote_plugins',
-  'spellfile_plugin',
-  '2html_plugin'
-}
-
-for _, i in pairs(disabled_built_ins) do
-  vim.g['loaded_' .. i] = 1
-end
-
 require("BlakeJC94.plugins")
 require("BlakeJC94.options")
 require("BlakeJC94.commands")
