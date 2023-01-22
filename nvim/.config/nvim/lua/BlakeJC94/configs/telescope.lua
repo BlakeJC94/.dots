9373e39047149dda041f396191fdc37721fae7e7
BlakeJC94 <blakejamescook@gmail.com>
2023-01-22 11:00:42 +1100

    update telescope

--- nvim/.config/nvim/lua/BlakeJC94/configs/telescope.lua
+++ nvim/.config/nvim/lua/BlakeJC94/configs/telescope.lua
@@ -13,27 +13,8 @@ M.config = function()
     local empty_borderchars = { " ", " ", " ", " ", " ", " ", " ", " " }
 
     telescope.setup({
-        extensions = {
-            fzf = {
-                fuzzy = true,
-                override_generic_sorter = true,
-                override_file_sorter = true,
-                case_mode = "smart_case",
-            },
-            file_browser = {
-                layout_config = {
-                    width = 60,
-                },
-                previewer = false,
-                hijack_netrw = true,
-                path = "%:p:h",
-                grouped = true,
-                hidden = true,
-                display_stat = false,
-                file_ignore_patterns = { "%/.git/", "%/__pycache__/" },
-            },
-        },
         defaults = {
+            theme = "ivy",
             borderchars = empty_borderchars,
             mappings = telescope_mappings,
             file_ignore_patterns = { "%.git/", "%/__pycache__/", "%/data/" },
@@ -41,6 +22,8 @@ M.config = function()
         pickers = {
             find_files = {
                 hidden = true,
+                theme = "ivy",
+                borderchars = empty_borderchars,
             },
             git_status = {
                 git_icons = {
@@ -60,6 +43,28 @@ M.config = function()
             spell_suggest = {
                 theme = "ivy",
                 borderchars = empty_borderchars,
+            },
+        },
+        extensions = {
+            fzf = {
+                fuzzy = true,
+                override_generic_sorter = true,
+                override_file_sorter = true,
+                case_mode = "smart_case",
+            },
+            file_browser = {
+                theme = "ivy",
+                borderchars = empty_borderchars,
+                -- layout_config = {
+                --     width = 60,
+                -- },
+                previewer = false,
+                hijack_netrw = true,
+                path = "%:p:h",
+                grouped = true,
+                hidden = true,
+                display_stat = false,
+                file_ignore_patterns = { "%/.git/", "%/__pycache__/" },
             },
         },
     })
