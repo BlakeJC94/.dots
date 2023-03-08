f2f1d307dd33ab2258a0eefa3410061857baa4c1
BlakeJC94 <blakejamescook@gmail.com>
2023-03-09 07:06:03 +1100

    sync

--- nvim/.config/nvim/lua/BlakeJC94/mappings.lua
+++ nvim/.config/nvim/lua/BlakeJC94/mappings.lua
@@ -24,14 +24,8 @@ return {
         -- Better opening of URLs and files
         ["gx"] = [[:silent execute '!xdg-open ' . shellescape(expand('<cfile>'), 1)<CR>]],
         -- Navigate git changes in file
-        [']h'] = ':silent Gitsigns next_hunk<CR>',
-        ['[h'] = ':silent Gitsigns prev_hunk<CR>',
-        -- Insert lines
-        ['[<CR>'] = 'mz:norm! O<CR>`z',
-        [']<CR>'] = 'mz:norm! o<CR>`z',
-        -- Yank and put current code block
-        ['[c'] = 'yap}p',
-        [']c'] = 'yap{p',
+        [']c'] = ':silent Gitsigns next_hunk<CR>',
+        ['[c'] = ':silent Gitsigns prev_hunk<CR>',
         -- Navigate diagnostics
         [']d'] = function() vim.diagnostic.goto_next() end,
         ['[d'] = function() vim.diagnostic.goto_prev() end,
