22e0dded07cd82df1595c8a34308dd4cd2d5e62f
BlakeJC94 <blakejamescook@gmail.com>
2023-03-10 11:47:11 +1100

    sync

--- nvim/.config/nvim/lua/BlakeJC94/configs/treesj.lua
+++ nvim/.config/nvim/lua/BlakeJC94/configs/treesj.lua
@@ -1,9 +1,25 @@
 local M = {}
 
-M.requires = { 'nvim-treesitter' }
+M.requires = { 'nvim-treesitter', "AndrewRadev/splitjoin.vim", }
 
 M.config = function()
     require('treesj').setup({ use_default_keymaps = false })
+
+    local langs = require('treesj.langs')['presets']
+
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = '*',
+        callback = function()
+            local opts = { buffer = true }
+            if langs[vim.bo.filetype] then
+                vim.keymap.set('n', 'gS', '<Cmd>TSJSplit<CR>', opts)
+                vim.keymap.set('n', 'gJ', '<Cmd>TSJJoin<CR>', opts)
+            else
+                vim.keymap.set('n', 'gS', '<Cmd>SplitjoinSplit<CR>', opts)
+                vim.keymap.set('n', 'gJ', '<Cmd>SplitjoinJoin<CR>', opts)
+            end
+        end,
+    })
 end
 
 return M
