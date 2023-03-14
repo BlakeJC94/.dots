2621479a9881fed7be7e3d5e5be12701ebb60837
BlakeJC94 <blakejamescook@gmail.com>
2023-03-14 15:16:22 +1100

    sync

--- nvim/.config/nvim/lua/BlakeJC94/configs/lsp_signature_nvim.lua
+++ nvim/.config/nvim/lua/BlakeJC94/configs/lsp_signature_nvim.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 function M.config()
-    require "lsp_signature".setup({hint_enable=false, move_cursor_key = "<C-x>"})
+    require "lsp_signature".setup({
+        hint_enable = false,
+        handler_opts = { border = "none" },
+        move_cursor_key = "<C-x>",
+    })
 end
 
 return M
